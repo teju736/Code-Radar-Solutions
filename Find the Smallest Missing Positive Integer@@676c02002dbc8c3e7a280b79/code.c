ea58378cb83a56d33dae6dcd749ca1d08678a62b
teju736 <palakpaneer736@gmail.com>
2025-03-27 19:18:41 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
+
 int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    int arr2[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    int arr2[n]; // Temporary array for positive numbers
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    if(n==1){
-        if (arr[0]!=1){
-        printf("%d",1);}
-        else{
-            printf("%d",2);
+
+    if (n == 1) {
+        if (arr[0] != 1) {
+            printf("%d", 1);
+        } else {
+            printf("%d", 2);
         }
         return 0;
     }
-    int j=0;
-    for(int i=0;i<n;i++){
-        if (arr[i]>0){
-            arr2[j]=arr[i];
+
+    int j = 0;
+
+    // Filter positive numbers into arr2
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            arr2[j] = arr[i];
             ++j;
-         }
+        }
     }
-    if(j==0){
-        printf("%d",1);
+
+    if (j == 0) {
+        printf("%d", 1);
         return 0;
     }
-    int sum=0;
-    int min=arr2[0];
-    int max=arr2[0];
-    for(int i=0;i<j;i++){
-        sum+=arr2[i];
-        if (arr2[i]<min){
-            min=arr2[i];
-        }
-        else if(arr2[i]>max){
-            max=arr2[i];
+
+    // Sort arr2 to find the missing smallest positive number
+    for (int i = 0; i < j - 1; i++) {
+        for (int k = i + 1; k < j; k++) {
+            if (arr2[i] > arr2[k]) {
+                int temp = arr2[i];
+                arr2[i] = arr2[k];
+                arr2[k] = temp;
+            }
         }
     }
-    printf("%d %d %d",min,max,j);
-    // int sum=0;
-    // for(int i=min;i<max+2;i++){
-    //     sum2+=i;
-    // }
-    // printf("%d",sum2-sum);
+
+    // Find the missing positive integer
+    int missing = 1;
+    for (int i = 0; i < j; i++) {
+        if (arr2[i] == missing) {
+            missing++;
+        }
+    }
+
+    printf("%d", missing);
+    
     return 0;
 }
