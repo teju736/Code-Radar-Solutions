b47069f2220c7ed7fd07abfbd99d5f843eb3d49d
teju736 <palakpaneer736@gmail.com>
2025-04-08 16:29:55 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -2,27 +2,17 @@
 #include <string.h>
 #include <ctype.h>
 
-#include <string.h>
-#include <stdio.h>
+// Caesar Cipher function definition
+void caesarCipher(char* message, int shift, char* encrypted) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char ch = message[i];
 
-void caesarCipherEncrypt(char arr[][100], int n, int shift) {
-    for (int i = 0; i < n; i++) {
-        int len = strlen(arr[i]);
-
-        for (int j = 0; j < len; j++) {
-            if (arr[i][j] >= 'a' && arr[i][j] <= 'z') {
-                arr[i][j] = ((arr[i][j] - 'a' + shift) % 26) + 'a';
-            }
-            else if (arr[i][j] >= 'A' && arr[i][j] <= 'Z') {
-                arr[i][j] = ((arr[i][j] - 'A' + shift) % 26) + 'A';
-            }
-            // Optionally handle other characters if needed
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+            encrypted[i] = (ch - base + shift) % 26 + base;
+        } else {
+            encrypted[i] = ch; // Non-alphabetic characters are unchanged
         }
     }
-}
-
-void printArray(char arr[][100], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
-    }
+    encrypted[strlen(message)] = '\0'; // Null-terminate the encrypted string
 }
