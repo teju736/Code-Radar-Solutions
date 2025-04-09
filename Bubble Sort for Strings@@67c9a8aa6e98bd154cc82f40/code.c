7c3526ac7dac629fb9321995ee5c4f85eb80d46f
teju736 <palakpaneer736@gmail.com>
2025-04-09 17:59:08 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-// Bubble Sort function definition
-void bubbleSort(char input[][100], int n, char output[][100]) {
-    // Copy original array to output
-    for (int i = 0; i < n; i++) {
-        strcpy(output[i], input[i]);
-    }
-
-    // Perform Bubble Sort on output
+// Function to sort an array of strings using bubble sort
+void bubblesort(char arr[][100], int n) {
+    char temp[100];
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(output[j], output[j + 1]) > 0) {
-                char temp[100];
-                strcpy(temp, output[j]);
-                strcpy(output[j], output[j + 1]);
-                strcpy(output[j + 1], temp);
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                // Swap strings if they are in the wrong order
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[j + 1]);
+                strcpy(arr[j + 1], temp);
             }
         }
     }
 }
 
+// Function to print the array of strings
+void printArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
+
+// Main function where program execution starts
+int main() {
+    int n; // Variable to store the number of strings
+
+    // Read the number of strings from user input
+    scanf("%d", &n);
+
+    char arr[n][100]; // Declare an array of strings with max length 100
+
+    // Read the strings from user input
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    // Call the bubble sort function
+    bubblesort(arr, n);
+
+    // Display the sorted array of strings
+    printArray(arr, n);
+
+    return 0; // Indicate successful program termination
+}
